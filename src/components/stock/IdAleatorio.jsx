ea17be4d5642be08c8743bc3e619045e4d7a9bf1
Endrigo Valente <endrigo.valente@soulasalle.com.br>
2022-06-18 00:08:21 -0300

    criação da estrutura de paginas

--- src/components/stock/IdAleatorio.jsx
+++ src/components/stock/IdAleatorio.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import { useState } from "react";
 import Main from "../templates/Main";
 
 const headerProps = {
@@ -7,12 +7,12 @@ const headerProps = {
     subtitle:''
 }
 
-export default class IdAleatorio extends Component{
-
-    render(){
-        return(
-            <Main {...headerProps}>
-            </Main>
-        )
-    }
-}
+/*
+Crie um algoritmo que gere o seguinte padrão de 
+ID aleatório: XXXX-AAAA-BBBB-CCCC
+Onde os padrões XXXX, AAAA, BBBB e CCCC são alfanuméricos aleatórios
+O padrão é uma string: "XXXX-AAAA-BBBB-CCCC"
+O resultado deve ser armazenado em uma variável. Por exemplo:
+let id = generarId()
+id vale ~ abc1-bb12-234a-bcc2
+*/
