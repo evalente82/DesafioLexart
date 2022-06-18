5a1a4cabd29d84adb5565759915ef548a07647af
Endrigo Valente <endrigo.valente@soulasalle.com.br>
2022-06-17 23:56:59 -0300

    Criação da estrutura de navegação das tarefas

--- src/components/stock/Hashmap.jsx
+++ src/components/stock/Hashmap.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import { useState } from "react";
 import Main from "../templates/Main";
 
 const headerProps = {
@@ -7,12 +7,15 @@ const headerProps = {
     subtitle:''
 }
 
-export default class Hashmap extends Component{
+/*
+Crie um hashmap (ou um objeto simples em JavaScript) que contenha as funções 
+elementares de adição, subtração, multiplicação e divisão.
+Leve em consideração que a divisão não deve permitir dividendo 0
+A função sum permite um array como parâmetro de entrada e adiciona todos 
+os seus elementos.
+A função de subtração permite um array como parâmetro de entrada e 
+subtrai todos os seus elementos.
+Função de multiplicação - Idem
+A função de divisão aceita dois parâmetros: a e b.
 
-    render(){
-        return(
-            <Main {...headerProps}>
-            </Main>
-        )
-    }
-}
+*/
